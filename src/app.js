43093d9696cffc5b533610abe3c5e6fa69d4cf22
proghany <proghany@ebay.com>
2020-11-30 10:33:39 +0100

    create endpoint to show fizzbuzz string, use npm fizzbuzz package

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const fizzbuzzcontroller = require('./controllers/fizzbuzz-controller');
 
 module.exports = (() => {
-    app.use(bodyparser.json());
+    app.use(bodyParser.json());
 
     app.use(fizzbuzzcontroller);
     return app;
--- src/controllers/fizzbuzz-controller.js
+++ src/controllers/fizzbuzz-controller.js
@@ -1,1 +1,17 @@
-console.log("WORKING")
+const { Router } = require('express');
+const controller = Router();
+const fizzbuzz = require('fizzbuzz-module-cli');
+
+controller.get('/fizzbuzz/:num', async (req, res) => {
+  if (req.params.num === Number(req.params.num).toString()) {
+
+    if (req.params.num <= 0) {
+      return res.status(400).send('Please insert a number bigger than 0!');
+    }
+    const result = fizzbuzz(req.params.num);
+    return res.json(result);
+  }
+  return res.status(400).send('Please insert a number!')
+});
+
+module.exports = controller;
